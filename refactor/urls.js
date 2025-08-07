f9acf95bd4b4e200fc82fffb7234c67d625a983f
Sumitgitup <Sumitroy7august@gmail.com>
2025-08-07 09:26:27 +0530

    added more changes to getUrl function

--- refactor/urls.js
+++ refactor/urls.js
@@ -1,21 +1,26 @@
 // Refactor this code
 
-function getURL(dev = false, tasks = {}) {
+function getURL({dev = false, nextTasks = false, prevTasks = false} = {}) {
+    const basePath = "/tasks";
     const params = new URLSearchParams();
 
     if (dev) {
         params.set('status', 'ACTIVE');
-        params.set('dev', true);
+        params.set('dev', 'true');
         params.set('size', '20');
     }
 
-    if (tasks.nextTasks) {
+    if (nextTasks) {
         params.set('hasNext', 'true');
     }
-    if (tasks.prevtasks) {
-        params.set('hasPrev', 'true')
+    if (prevTasks) {
+        params.set('hasPrev', 'true');
     }
+
     const query = params.toString();
-    const url = query ? `/tasks?$query` : '/tasks';
+    const url = query ? `/${basePath}?${query}` : basePath;
     return {url}
 }
+
+
+console.log( getURL());
